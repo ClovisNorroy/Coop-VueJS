d7dff1ae233aa5fb713c0ce34a86faec31ca1d72
Clovis <clovis.norroy@gmail.com>
2020-02-15 17:24:21 +0100

    Fixed: Display last member messages

--- src/components/ListeMessages.vue
+++ src/components/ListeMessages.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="listeMessages">
-        <div class="message" v-for="message in messages">
+        <div class="message" v-for="message in messages" v-if="messages.length">
+            <div class="context" v-if="displayContext"> {{message.channel.label}} </div>
             {{  message.message }}
         </div>
         <poster-message v-if="channel" :messages="messages" :channel-id="channel"></poster-message>
@@ -11,7 +12,7 @@
 import PosterMessage from "./PosterMessage";
     export default {
         name: "ListeMessages",
-        props: ['channel', 'messages'],
+        props: ['channel', 'messages', 'displayContext'],
         components:{
             PosterMessage
         },
--- src/views/DetailMembre.vue
+++ src/views/DetailMembre.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="detailMembre">
         {{membre}}
-        <liste-messages :messages="memberLastMessages"></liste-messages>
+        <liste-messages :messages="memberLastMessages" display-context="true"></liste-messages>
     </div>
 
 </template>
@@ -23,14 +23,11 @@
             }
         },
         mounted() {
-            this.getMemberLastMessages();
+            this.keepNLastMessages(4);
         },
         methods:{
             //TODO : add to local storage and delete on new message
-            getMemberLastMessages: async function() {
-                this.keepNLastMessages(4, this.getAllMessages());
-            },
-            getAllMessages: function(){
+            getAllMessages: async function(){
                 let messageList = [];
                 axios.get('channels').then(channelResponse => {
                     channelResponse.data.forEach(channel=>{
@@ -42,18 +39,47 @@
                         }).catch(error => { console.log(error.response)})
                     });
                 }).catch(error => { console.log(error.response)});
-                // this.keepNLastMessages(10, messageList);
                 return messageList;
             },
-            keepNLastMessages: function(nbrMessagesToKeep, listMessages){
+            keepNLastMessages:async function(nbrMessagesToKeep){
+                let listMessages = await this.getAllMessages();
+                let channelMap = [];
                 setTimeout(function(){
-                    listMessages.sort(
-                        function(a,b) {
-                            let dateA = new Date(a.created_at); let dateB = new Date(b.created_at);
-                            return dateA-dateB;
-                        })}, 1000);
-                this.memberLastMessages = listMessages;
-            },
+                    axios.get('channels').then(response => {
+                        response.data.forEach(channel =>{
+                            channelMap[channel.id] = {label: channel.label, topic: channel.topic}
+                        });
+                        listMessages.forEach(message => {
+                            message.channel = channelMap[message.channel_id];
+                        });
+                        listMessages.sort(
+                            function(a,b) {
+                                let dateA = new Date(a.created_at); let dateB = new Date(b.created_at);
+                                return dateA-dateB;
+                            }
+                        );
+                    });
+                }, 500);
+                setTimeout(()=>{
+                    this.memberLastMessages = listMessages.slice(nbrMessagesToKeep);
+                }, 1000);
+                // axios.get('channels').then(response => {
+                //     response.data.forEach(channel =>{
+                //         channelMap[channel.id] = {label: channel.label, topic: channel.topic}
+                //     });
+                //     listMessages.forEach(message => {
+                //         message.channel = channelMap[message.channel_id];
+                //     });
+                //     listMessages.sort(
+                //         function(a,b) {
+                //             console.log("oui");
+                //             let dateA = new Date(a.created_at); let dateB = new Date(b.created_at);
+                //             return dateA-dateB;
+                //         }
+                //     );
+                //     this.memberLastMessages = listMessages;
+                // });
+            }
         }
     }
 </script>
